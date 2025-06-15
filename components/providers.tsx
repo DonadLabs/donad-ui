760546a1300b4f9d8250424f7ef9ad8c3f8398e6
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-06-15 15:36:24 +0700

    add monadTesnet

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,14 +3,14 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, base, sepolia } from 'wagmi/chains';
+import { monadTestnet } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 // Configure chains and providers
 const config = getDefaultConfig({
   appName: 'Donad', 
   projectId: '322a6a0a1a7cf83e48bac5da6b77d3e2', // Replace with your actual Project ID
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [monadTestnet],
   ssr: true,
 });
 
